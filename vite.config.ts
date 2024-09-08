ebf81ee299cd6ed68dd206536b29703fa3a98420
ridham <ridhamsuhagiya@gmail.com>
2024-09-08 14:35:03 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://sustainable-solutions-23.netlify.app/",
+  base: "/",
   build: {
     outDir: 'dist',
     rollupOptions: {
